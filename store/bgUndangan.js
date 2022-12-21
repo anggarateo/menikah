6cc70fb14903eddba70690f03c8c322905351ef0
Teo Anggara <admin.admin@ilcs.co.id>
2022-12-21 19:10:07 +0700

    update ./image

--- store/bgUndangan.js
+++ store/bgUndangan.js
@@ -1,15 +1,15 @@
 export const state = () => ({
     'webp': [
-        '/bg-undangan/DSC00063.webp',
-        '/bg-undangan/DSC00076.webp',
-        '/bg-undangan/DSC00109.webp',
-        '/bg-undangan/DSC00151.webp',
-        '/bg-undangan/DSC00161.webp',
-        '/bg-undangan/DSC00164.webp',
-        '/bg-undangan/DSC00167.webp',
+        './bg-undangan/DSC00063.webp',
+        './bg-undangan/DSC00076.webp',
+        './bg-undangan/DSC00109.webp',
+        './bg-undangan/DSC00151.webp',
+        './bg-undangan/DSC00161.webp',
+        './bg-undangan/DSC00164.webp',
+        './bg-undangan/DSC00167.webp',
     ],
     'mempelai': {
-        'putra': '/bg-undangan/DSC00109.webp',
-        'putri': '/bg-undangan/DSC00109.webp'
+        'putra': './bg-undangan/DSC00109.webp',
+        'putri': './bg-undangan/DSC00109.webp'
     }
 })
