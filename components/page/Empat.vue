f07fe0317b7f70b10df332a4ed956679be3afee3
Teo Anggara <admin.admin@ilcs.co.id>
2022-12-21 11:46:21 +0700

    empat.vue img to bg-image, add bg to static

--- components/page/Empat.vue
+++ components/page/Empat.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="w-full h-full">
         <ClientOnly>
-            <carousel :per-page="1" :autoplay="true" :loop="true" :speed="3000" :autoplay-timeout="6000"
-                :autoplay-hover-pause="false">
+            <carousel :per-page="1" :autoplay="true" :loop="true" :pagination-enabled="false" :speed="3000"
+                :autoplay-timeout="6000" :autoplay-hover-pause="false">
                 <slide v-for="(image, i) in images" :key="i">
-                    <nuxt-img :src="image" sizes="sm:100vw md:25vw" loading="lazy" @click="index = i" />
+                    <div class="w-full h-screen bg-center bg-contain bg-no-repeat cursor-pointer"
+                        :style="{ backgroundImage: `url(${image})` }" @click="index = i">
+                    </div>
                 </slide>
             </carousel>
             <vgs :images="images" :index="index" @close="index = null" />
@@ -22,13 +24,13 @@ export default {
     data() {
         return {
             images: [
-                'DSC00063.webp',
-                'DSC00076.webp',
-                'DSC00109.webp',
-                'DSC00151.webp',
-                'DSC00161.webp',
-                'DSC00164.webp',
-                'DSC00167.webp',
+                '/bg-undangan/DSC00063.webp',
+                '/bg-undangan/DSC00076.webp',
+                '/bg-undangan/DSC00109.webp',
+                '/bg-undangan/DSC00151.webp',
+                '/bg-undangan/DSC00161.webp',
+                '/bg-undangan/DSC00164.webp',
+                '/bg-undangan/DSC00167.webp',
             ],
             index: null
         }
