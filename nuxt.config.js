e245cdcbf79cb17cbc34f0c7778d0107dcbc9829
Teo Anggara <admin.admin@ilcs.co.id>
2022-12-22 14:18:54 +0700

    add tailwindcss nuxtconfig

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,6 +44,10 @@ export default {
     '@nuxtjs/tailwindcss'
   ],
 
+  tailwindcss: {
+    cssPath: '~/plugins/tailwind.css'
+  },
+
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
